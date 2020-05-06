fb9d0f7c2f9bc096e92b4d052bb07521f08a3f0e
Eva Longoria Lopez <ebita@hotmail.es>
2020-05-06 21:11:57 +0200

    configuracion pitar displays

--- reloj/reloj.ino
+++ reloj/reloj.ino
@@ -1,22 +1,10 @@
-//Etiquetas
+//Librerías
 
-#define INITIAL_YEARS 2020
-#define INITIAL_MONTH 4
-#define INITIAL_DAYS 27
-#define INITIAL_HOURS 3
-#define INITIAL_MINUTES 57
-#define INITIAL_SECONDS 0
-#define INITIAL_MILLISECONDS 0
-#define INTERVAL_MILLISECONDS_CLOCK 10
-#define INTERVAL_BUTTON_MILLISECONDS 200
-#define INTERVAL_MILLISECONDS_CHRONOMETER 10
-#define INTERVAL_RESET 2000
-#define INTERVAL_MILLISECONDS_COUNTDOWN 10
-#define CLOCK_MODE 1
-#define CHRONOMETER_MODE 2
-#define COUNTDOWN_MODE 3
-#define CHRONOMETER_START_MODE 4
-#define CHRONOMETER_STOP_MODE 5
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h>
+#endif
+
 
 //Etiquetas pines
 
@@ -24,6 +12,11 @@
 #define BUTTON_DOWN_PIN 7
 #define BUTTON_START_STOP_PIN 8
 #define BUTTON_MODE_PIN 2
+#define TENTHS_DISPLAY_PIN 6
+#define SECONDS_DISPLAY_PIN 10
+#define MINUTES_DISPLAY_PIN 5
+#define HOURS_DISPLAY_PIN 3
+#define DOTS_DISPLAY_PIN 9
 
 
 //Variables
@@ -66,6 +59,52 @@ unsigned long lastTimeReadButtons;
 unsigned long lastTimeCountDown;
 unsigned long lastTimeForResetCountDown;
 unsigned long elapsedTimeCountDown;
+Adafruit_NeoPixel displayTenths = Adafruit_NeoPixel(21, TENTHS_DISPLAY_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel displaySeconds = Adafruit_NeoPixel(42, SECONDS_DISPLAY_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel displayMinutes = Adafruit_NeoPixel(42, MINUTES_DISPLAY_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel displayHours = Adafruit_NeoPixel(42, HOURS_DISPLAY_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel displayDots = Adafruit_NeoPixel(5, DOTS_DISPLAY_PIN, NEO_GRB + NEO_KHZ800);
+
+
+//Constantes
+
+const int INITIAL_YEARS = 2020;
+const byte INITIAL_MONTH = 4;
+const byte INITIAL_DAYS = 27;
+const byte INITIAL_HOURS = 3;
+const byte INITIAL_MINUTES = 57;
+const byte INITIAL_SECONDS = 0;
+const int INITIAL_MILLISECONDS = 0;
+const unsigned long INTERVAL_MILLISECONDS_CLOCK = 10;
+const unsigned long INTERVAL_BUTTON_MILLISECONDS = 200;
+const unsigned long INTERVAL_MILLISECONDS_CHRONOMETER = 10;
+const unsigned long INTERVAL_RESET = 2000;
+const unsigned long INTERVAL_MILLISECONDS_COUNTDOWN = 10;
+const byte CLOCK_MODE = 1;
+const byte CHRONOMETER_MODE = 2;
+const byte COUNTDOWN_MODE = 3;
+const byte CHRONOMETER_START_MODE = 4;
+const byte CHRONOMETER_STOP_MODE = 5;
+const byte LITTLE_ZERO[21] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0};
+const byte LITTLE_ONE[21] = {1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1};
+const byte LITTLE_TWO[21] = {0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+const byte LITTLE_THREE[21] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1};
+const byte LITTLE_FOUR[21] = {0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1};
+const byte LITTLE_FIVE[21] = {1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1};
+const byte LITTLE_SIX[21] = {1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+const byte LITTLE_SEVEN[21] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+const byte LITTLE_EIGHT[21] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+const byte LITTLE_NINE[21] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1};
+const byte BIG_ZERO[42] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0};
+const byte BIG_ONE[42] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1};
+const byte BIG_TWO[42] = {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+const byte BIG_THREE[42] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1};
+const byte BIG_FOUR[42] = {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+const byte BIG_FIVE[42] = {1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+const byte BIG_SIX[42] = {1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+const byte BIG_SEVEN[42] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+const byte BIG_EIGHT[42] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+const byte BIG_NINE[42] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
 
 
 //Métodos
@@ -98,6 +137,15 @@ void setup() {
   pinMode(BUTTON_DOWN_PIN, INPUT_PULLUP);
   pinMode(BUTTON_START_STOP_PIN, INPUT_PULLUP);
   pinMode(BUTTON_MODE_PIN, INPUT_PULLUP);
+#if defined (__AVR_ATtiny85__)
+  if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+#endif
+  // End of trinket special code
+  displayTenths.begin(); // This initializes the NeoPixel library.
+  displaySeconds.begin();
+  displayMinutes.begin();
+  displayHours.begin();
+  displayDots.begin();
 }
 
 
@@ -272,6 +320,87 @@ void checkStartStop() {
 }
 
 
+void checkUp() {
+  if (buttonUp == HIGH) {
+    if (mode == CLOCK_MODE) {
+
+    }
+    if (mode == COUNTDOWN_MODE) {
+
+    }
+  }
+}
+
+void checkDown() {
+  if (buttonDown == HIGH) {
+    if (mode == CLOCK_MODE) {
+
+    }
+    if (mode == COUNTDOWN_MODE) {
+
+    }
+  }
+}
+
+void drawDoubleDisplay(Adafruit_NeoPixel displayToDraw, int numberDisplay, bool isBig) {
+  byte firstDigit;
+  byte secondDigit;
+
+  if (numberDisplay >= 0 && numberDisplay < 10) {
+    firstDigit = 0;
+    secondDigit = numberDisplay;
+  }
+  else if (numberDisplay >= 10) {
+    String numberDisplayStr = String(numberDisplay);
+    firstDigit = String(numberDisplayStr[0]).toInt();
+    secondDigit = String(numberDisplayStr[1]).toInt();
+  }
+  drawDisplay(displayToDraw, firstDigit, isBig, 1);
+  drawDisplay(displayToDraw, secondDigit, isBig, 2);
+}
+
+
+void drawDisplay (Adafruit_NeoPixel displayToDraw, byte drawDigit, bool isBig, byte positionToDraw) {
+  if (isBig) {
+    byte numberToDraw[42];
+    getDrawNumber(drawDigit, isBig, numberToDraw);
+    for (int i = 0; i < 42; i++) {
+      if (numberToDraw[i] == 1) {
+        int pixelOn;
+        if (positionToDraw == 1) {
+          pixelOn = i;
+        }
+        if (positionToDraw == 2) {
+          pixelOn = i + 42;
+        }
+        displayToDraw.setPixelColor(pixelOn, displayToDraw.Color(23, 161, 165)); // Moderately bright green color.
+        displayToDraw.setBrightness(5);
+        displayToDraw.show();// This sends the updated pixel color to the hardware.
+      }
+    }
+  }
+  else {
+    byte numberToDraw[21];
+    getDrawNumber(drawDigit, isBig, numberToDraw);
+    for (int i = 0; i < 21; i++) {
+      if (numberToDraw[i] == 1) {
+        int pixelOn;
+        if (positionToDraw == 1) {
+          pixelOn = i;
+        }
+        if (positionToDraw == 2) {
+          pixelOn = i + 21;
+        }
+        displayToDraw.setPixelColor(pixelOn, displayToDraw.Color(23, 161, 165)); // Moderately bright green color.
+        displayToDraw.setBrightness(5);
+        displayToDraw.show();// This sends the updated pixel color to the hardware.
+      }
+    }
+  }
+
+}
+
+
 void calculateCountDown() {
   if (hoursCountDown == 0 && minutesCountDown == 0 && secondsCountDown == 0) {
     isCountDownRunning = false;
@@ -290,7 +419,6 @@ void calculateCountDown() {
     hoursCountDown--;
     minutesCountDown = 59;
   }
-
 }
 
 
@@ -386,88 +514,12 @@ void drawChronometer() {
   Serial.println(output);
 }
 
-void drawClock() {
-  String output = "";
-  String yearsStr;
-  String monthStr;
-  String daysStr;
-  String hoursStr;
-  String minutesStr;
-  String secondsStr;
-  String millisecondsStr;
-  int dayOfWeek = getDayOfWeek(years, month, days);
-
-  if (years < 10) {
-    yearsStr = "000" + String(years);
-  }
-  else if (years < 100) {
-    yearsStr = "00" + String(years);
-  }
-  else if (years < 1000) {
-    yearsStr = "0" + String(years);
-  }
-
-  else {
-    yearsStr = String(years);
-  }
-
-  if (month < 10) {
-    monthStr = "0" + String(month);
-  }
-  else {
-    monthStr = String(month);
-  }
-
-  if (days < 10) {
-    daysStr = "0" + String(days);
-  }
-  else {
-    daysStr = String(days);
-  }
-
-
-  if (hours < 10) {
-    hoursStr = "0" + String(hours);
-  }
-  else {
-    hoursStr = String(hours);
-  }
-
-  if (minutes < 10) {
-    minutesStr = "0" + String(minutes);
-  }
-  else {
-    minutesStr = String(minutes);
-  }
-
-  if (seconds < 10) {
-    secondsStr = "0" + String(seconds);
-  }
-  else {
-    secondsStr = String(seconds);
-  }
-
-  if (milliseconds < 100) {
-    millisecondsStr = "0" + String(milliseconds / 10);
-  }
-  else {
-    millisecondsStr = String(milliseconds / 10);
-  }
-
-  output = String(week[dayOfWeek - 1]) + "  " + String(monthOfYears[month - 1]) + "  " + daysStr + "/" + monthStr + "/" + yearsStr + "  " + hoursStr + ":" + minutesStr + ":" + secondsStr + "." + millisecondsStr;
-  // output = hoursStr + ":" + minutesStr + ":" + secondsStr + "." + millisecondsStr;
-
-  Serial.println(output);
-}
-
 
 void calculateClockTime() {
   byte daysMonths = daysOfMonths[month - 1];
   if (month == 2 && isLeapYears(years)) {
     daysMonths = 29;
-
   }
-
   milliseconds += elapsedTimeClock;
   if (milliseconds >= 1000) {
     seconds++;
@@ -494,6 +546,13 @@ void calculateClockTime() {
     month = 1;
   }
 }
+
+
+void drawClock() {
+  drawDoubleDisplay(displayTenths, milliseconds, false);
+
+}
+
 
 /** documentacion metodo
    calcula si el año es bisiesto
@@ -524,3 +583,94 @@ int getDayOfWeek(int years, byte month, byte days) {
     return dayAux;
   }
 }
+
+
+void getDrawNumber(byte drawDigit, bool isBig, byte *number) {
+
+  switch (drawDigit) {
+    case 0:
+      if (isBig) {
+        number = BIG_ZERO;
+      }
+      else {
+        number = LITTLE_ZERO;
+      }
+      break;
+
+    case 1:
+      if (isBig) {
+        number = BIG_ONE;
+      }
+      else {
+        number = LITTLE_ONE;
+      }
+      break;
+    case 2:
+      if (isBig) {
+        number = BIG_TWO;
+      }
+      else {
+        number = LITTLE_TWO;
+      }
+      break;
+    case 3:
+      if (isBig) {
+        number = BIG_THREE;
+      }
+      else {
+        number = LITTLE_THREE;
+      }
+      break;
+    case 4:
+      if (isBig) {
+        number = BIG_FOUR;
+      }
+      else {
+        number = LITTLE_FOUR;
+      }
+      break;
+    case 5:
+      if (isBig) {
+        number = BIG_FIVE;
+      }
+      else {
+        number = LITTLE_FIVE;
+      }
+      break;
+    case 6:
+      if (isBig) {
+        number = BIG_SIX;
+      }
+      else {
+        number = LITTLE_SIX;
+      }
+      break;
+    case 7:
+      if (isBig) {
+        number = BIG_SEVEN;
+      }
+      else {
+        number = LITTLE_SEVEN;
+      }
+      break;
+    case 8:
+      if (isBig) {
+        number = BIG_EIGHT;
+      }
+      else {
+        number = LITTLE_EIGHT;
+      }
+      break;
+    case 9:
+      if (isBig) {
+        number = BIG_NINE;
+      }
+      else {
+        number = LITTLE_NINE;
+      }
+      break;
+  }
+
+}
+//rebiba digito a pintar(parametro)
+//booleano is big(parametro)
