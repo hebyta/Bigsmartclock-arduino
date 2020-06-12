b5c332608d34a794af2425880331db5d96fa7243
Juan José Longoria <juan.longoria.lopez@accenture.com>
2020-06-12 11:09:54 +0200

    Corrigiendo el desfase temporal

--- reloj/reloj.ino
+++ reloj/reloj.ino
@@ -29,7 +29,7 @@ const byte INITIAL_CLOCK_SECONDS = 0;
 const byte INITIAL_CLOCK_MINUTES = 0;
 const byte INITIAL_CLOCK_HOURS = 12;
 const int SENDING_DATA_INTERVAL = 500;
-const byte TIME_OFFSET_MILLIS = 2;
+const byte TIME_OFFSET_MILLIS = 1;
 const byte TIME_OFFSET_MODULE = 2;
 
 // Constantes de modo
